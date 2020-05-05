a7c79a2f1e7eae98281d42e27162776728678fde
Ken Holston <git.kholston@gmail.com>
2020-05-05 14:35:10 -0400

    Define the cell class

--- Cell.rb
+++ Cell.rb
@@ -1,7 +1,9 @@
 module TicTacToe
+  # A class that defines a cell on the game board
   class Cell
-    def initialize
-      
+    attr_acessor :value
+    def initialize(value = '')
+      @value = value
     end
   end
 end
